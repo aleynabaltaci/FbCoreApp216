88c953cefccce9e778688d09aa6b33822f602cf3
aleyna baltacı <aleynabaltaci@outlook.com>
2022-03-21 20:25:24 +0300

    AppdataContext

---
+++ FbCoreApp216.Core/UnitOfWork/IUnitOfWork.cs
@@ -0,0 +1,18 @@
+﻿using FbCoreApp216.Core.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FbCoreApp216.Core.UnitOfWork
+{
+    public interface IUnitOfWork
+    {
+        IProductRepository product { get; }
+        ICategoryRepository category { get; }
+
+        Task CommitAsync();
+        void Commit();
+    }
+}
