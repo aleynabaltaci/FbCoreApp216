f08d1525ea78f39b9d6a863a0becb4da77f0950c
aleyna baltacı <aleynabaltaci@outlook.com>
2022-03-21 18:47:52 +0300

    Add project files.

---
+++ FbCoreApp216.Core/Models/Category.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FbCoreApp216.Core.Models
+{
+    public class Category:BaseEntity
+    {
+        public Category()
+        {
+            //Products=new Collection<Product>();
+        }
+        public string CategoryName { get; set; }
+        public bool IsDeleted { get; set; }
+
+
+        public virtual ICollection<Product> Products { get; set; } = new Collection<Product>();
+    }
+}
